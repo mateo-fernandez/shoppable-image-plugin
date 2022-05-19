8b75773b7ad33972524b10201fea892df10f0dd7
Matéo Fernandez <m.fernandez@choosit.com>
2022-05-19 16:56:25 +0200

    v0: example of img/products

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ const img0 = document.querySelector('#img-0');
 createShoppableImage(img0, 'http://localhost:8080/config_img_0.json');
 
 const img1 = document.querySelector('#img-1');
-createShoppableImage(img1, 'http://localhost:8080/config_img_0.json');
+createShoppableImage(img1, 'http://localhost:8080/config_img_1.json');
